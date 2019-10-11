6e5326bc4c77be6a597913812af050b14dc10dd1
Thomas Kibaru <kibtom@yahoo.com>
2019-10-11 11:24:52 +0100

    Add data annoitations

--- HealthCare/Models/FeedbackForm.cs
+++ HealthCare/Models/FeedbackForm.cs
@@ -8,12 +8,29 @@ namespace HealthCare.Models
 {
     public class FeedbackForm
     {
+        [Display(Name = "Your Name")]
+        [StringLength(40, ErrorMessage = "{0} cannot be longer than 40 characters.")]
         public string UserName { get; set; }
-        [Required]
+
+        
+        [Display(Name = "Your Postcode")]
+        [Required(ErrorMessage = "{0} is a required.")]
+        [StringLength(12, ErrorMessage = "{0} cannot be longer than 12 characters.")]
         public string UserPostcode { get; set; }
-        [Required]
+
+        
+        [Display(Name = "Your Email Address")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        [StringLength(40, ErrorMessage = "{0} cannot be longer than 40 characters.")]
         public string UserEmailAddress { get; set; }
+
+        [Display(Name = "Your Phone Number")]
+        [MaxLength(15, ErrorMessage = "{0} cannot be longer than 15 characters.")]
         public int UserPhoneNumber { get; set; }
+
+        [Display(Name = "Your Message")]
+        [StringLength(1000, ErrorMessage = "{0} cannot be longer than 1000 characters.")]
         public string UserMessage { get; set; }
     }
 }
