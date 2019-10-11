e899f44775a0433bfa81c3fe224d695501bbe087
Thomas Kibaru <kibtom@yahoo.com>
2019-10-11 11:30:56 +0100

    Update Phone number validations

--- HealthCare/Models/FeedbackForm.cs
+++ HealthCare/Models/FeedbackForm.cs
@@ -26,7 +26,7 @@ namespace HealthCare.Models
         public string UserEmailAddress { get; set; }
 
         [Display(Name = "Your Phone Number")]
-        [MaxLength(15, ErrorMessage = "{0} cannot be longer than 15 characters.")]
+        [StringLength(15, ErrorMessage = "{0} cannot be longer than 15 characters.")]
         public int UserPhoneNumber { get; set; }
 
         [Display(Name = "Your Message")]
